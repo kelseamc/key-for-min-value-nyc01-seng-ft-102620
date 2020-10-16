4e3a0b9d580c543dfa7ad3969be2d03507356503
Kelsea McAllister <kelsea.mcall@gmail.com>
2020-10-16 14:29:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,11 @@
 
 def key_for_min_value(name_hash)
   min_value = name_hash[0]
-  min_key = nil
+  min_key = []
   name_hash.each do |key, value|
     if value <= min_value
       min_value = value
-      min_key = key
+      min_key << key
     end
   end
   min_key
