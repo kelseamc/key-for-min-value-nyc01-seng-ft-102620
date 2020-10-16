3e64df8dd7b59cd0243b9cf6eb569c623a44784d
Kelsea McAllister <kelsea.mcall@gmail.com>
2020-10-16 14:06:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
+  min_value = ""
+  name_hash.each do |key, value|
+    if value < min_value.to_i
+      min_value = "#{value}"
+      return key
+    end
+  end 
 end
