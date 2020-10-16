db877d93dda7b80b8cbad4f625bea4d071fa425d
Kelsea McAllister <kelsea.mcall@gmail.com>
2020-10-16 13:59:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min_value = []
   min_key = ""
   name_hash.each do |key, value|
-    if value < min_value
+    if value < min_value[0]
       min_value[0] = value
       min_key = key
     end
